6a8b8117a736ddae29ab7ee717e257084f49c885
Alexandre <alexandremoraiscosta@outlook.com>
2026-02-24 18:45:57 -0300

    test: mock dependencies in users unit tests

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  const userModelMock = {
+    create: jest.fn(),
+    find: jest.fn(() => ({ exec: jest.fn() })),
+    findById: jest.fn(() => ({ exec: jest.fn() })),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: userModelMock,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
